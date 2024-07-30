334c0ac5d401926538c3cd1de5b5e7342cba9d55
Hitusss <kuba.niespielak@gmail.com>
2024-07-30 14:16:41 +0200

    feat: add @ianvs/prettier-plugin-sort-imports

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,27 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^~/lib/(.*)$",
+    "^~/components/ui/(.*)$",
+    "^~/components/(.*)$",
+    "^~/styles/(.*)$",
+    "^~/app/(.*)$",
+    "^~/(.*)$",
+    "",
+    "^#(.*)$",
+    "",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
 };
 
 export default config;
