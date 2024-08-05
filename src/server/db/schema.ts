a3756a92f8c34018c0eb8c0e09063e2ca01a7cbd
Hitusss <kuba.niespielak@gmail.com>
2024-08-05 16:34:00 +0200

    style: remove some comments

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -11,12 +11,6 @@ import {
 } from "drizzle-orm/pg-core";
 import { type AdapterAccount } from "next-auth/adapters";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `just-todo_${name}`);
 
 export const posts = createTable(
