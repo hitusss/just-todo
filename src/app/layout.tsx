5f11c59af4f60ecd27f735eaaec71913fdcbf11a
Hitusss <kuba.niespielak@gmail.com>
2024-08-22 13:48:45 +0200

    fix: add suppressHydrationWarning to html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${GeistSans.variable} h-full overflow-x-hidden`}
+      suppressHydrationWarning
     >
       <body>
         <ThemeProvider
