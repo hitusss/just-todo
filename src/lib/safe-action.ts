b040cbd1474e2ada64cab989311fb0a8075e4b7a
Hitusss <kuba.niespielak@gmail.com>
2024-08-31 20:42:32 +0200

    feat: add authActionClient

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,3 +1,19 @@
 import { createSafeActionClient } from "next-safe-action";
 
+import { auth } from "~/server/auth";
+
 export const actionClient = createSafeActionClient();
+
+export const authActionClient = actionClient.use(async ({ next }) => {
+  const session = await auth();
+
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+
+  return next({
+    ctx: {
+      session,
+    },
+  });
+});
