e83bd7dfc2180b5ca8fe8512fc1e8a4e78cfe135
Hitusss <kuba.niespielak@gmail.com>
2024-08-31 17:37:53 +0200

    feat: add UserName and update user validator

--- src/validators/user.ts
+++ src/validators/user.ts
@@ -1,10 +1,30 @@
 import { z } from "zod";
 
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 40;
+
+export const USER_EMAIL_MIN_LENGTH = 3;
+export const USER_EMAIL_MAX_LENGTH = 255;
+
 export const EmailSchema = z
   .string({ required_error: "Email is required" })
   .email({ message: "Email is invalid" })
-  .min(3, { message: "Email is too short" })
-  .max(255, { message: "Email is too long" });
+  .min(USER_EMAIL_MIN_LENGTH, { message: "Email is too short" })
+  .max(USER_EMAIL_MAX_LENGTH, { message: "Email is too long" });
+
+export const UsernameSchema = z
+  .string({ required_error: "Username is required" })
+  .min(USERNAME_MIN_LENGTH, { message: "Username is too short" })
+  .max(USERNAME_MAX_LENGTH, { message: "Username is too long" })
+  .regex(/^[a-zA-Z0-9_]+$/, {
+    message: "Username can only include letters, numbers, and underscores",
+  })
+  .transform((value) => value.toLowerCase());
+
+export const NameSchema = z
+  .string({ required_error: "Name is required" })
+  .min(USERNAME_MIN_LENGTH, { message: "Name is too short" })
+  .max(USERNAME_MAX_LENGTH, { message: "Name is too long" });
 
 export const LoginWithEmailSchema = z.object({
   email: EmailSchema,
