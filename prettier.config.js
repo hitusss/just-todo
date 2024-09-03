83372ccd7596a1892a19fb46b083905e192deaf7
Hitusss <kuba.niespielak@gmail.com>
2024-09-03 16:29:34 +0200

    feat: update shadcn/ui config and change style and baseColor

--- prettier.config.js
+++ prettier.config.js
@@ -14,6 +14,7 @@ const config = {
     "^~/server/(.*)$",
     "^~/actions/(.*)$",
     "^~/lib/(.*)$",
+    "^~/hooks/(.*)$",
     "^~/components/ui/(.*)$",
     "^~/components/(.*)$",
     "^~/styles/(.*)$",
