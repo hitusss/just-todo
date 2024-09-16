aa19a467f7a19637973b30f8e12c4586bddcb0ae
Hitusss <kuba.niespielak@gmail.com>
2024-09-16 17:09:43 +0200

    feat: improve LogoutForm to accept Button props

--- src/components/auth/logout-form.tsx
+++ src/components/auth/logout-form.tsx
@@ -1,17 +1,29 @@
 "use client";
 
+import * as React from "react";
 import { LogOut } from "lucide-react";
 import { useAction } from "next-safe-action/hooks";
 
 import { logoutAction } from "~/actions/auth";
-import { Button } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+import { Button, type ButtonProps } from "~/components/ui/button";
 
-export function LogoutForm() {
+export const LogoutForm = React.forwardRef<
+  HTMLButtonElement,
+  Omit<ButtonProps, "children" | "asChild" | "onClick">
+>(({ className, variant = "destructive", ...props }, ref) => {
   const logout = useAction(logoutAction);
   return (
-    <Button onClick={() => logout.execute()} className="gap-2">
+    <Button
+      ref={ref}
+      onClick={() => logout.execute()}
+      variant={variant}
+      className={cn("gap-2", className)}
+      {...props}
+    >
       <LogOut className="h-4 w-4" />
       Logout
     </Button>
   );
-}
+});
+LogoutForm.displayName = "LogoutForm";
