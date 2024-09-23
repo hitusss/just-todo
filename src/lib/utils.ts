3cd80145245c08e0dcec3beb69ae4c19ccfcc732
Hitusss <kuba.niespielak@gmail.com>
2024-09-23 16:33:37 +0200

    feat: add getNameShortcut util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getNameShortcut(name: string) {
+  const parts = name.split(" ");
+  if (parts.length > 1) {
+    const first = parts[0]!;
+    const second = parts[parts.length - 1]!;
+    return `${first.slice(0, 1)}${second.slice(0, 1)}`.toUpperCase();
+  } else {
+    return name.slice(0, 2).toUpperCase();
+  }
+}
