c20d60b2014148224f73bb7c09c977d83a51f171
Hitusss <kuba.niespielak@gmail.com>
2024-09-23 16:38:31 +0200

    feat: add app route layout

---
+++ src/app/app/layout.tsx
@@ -0,0 +1,21 @@
+import { cookies } from "next/headers";
+
+import { SidebarLayout, SidebarTrigger } from "~/components/ui/sidebar";
+import { AppSidebar } from "~/components/app/sidebar";
+
+const SIDEBAR_STATE_COOKIE = "app:sidebar:state";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarLayout
+      sidebarStateCookie={SIDEBAR_STATE_COOKIE}
+      defaultOpen={cookies().get(SIDEBAR_STATE_COOKIE)?.value === "true"}
+    >
+      <AppSidebar />
+      <main className="flex flex-1 flex-col p-2 transition-all duration-300 ease-in-out">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarLayout>
+  );
+}
